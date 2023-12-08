48397f974859a4383cecb16d9ca03a5f0625ae1e
duncan <dmckee95@gmail.com>
2023-12-08 16:28:02 -0500

    Create and set up Cron Job

---
+++ config/cron-tasks.js
@@ -0,0 +1,20 @@
+module.exports = {
+    myJob:{
+        task: async ({strapi}) => {
+            const thirtyDaysAgo = new Date();
+            thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30)
+
+            const expiredJobs = await strapi.entityService.findMany("api::job.job", {
+               filters: {createdAt: thirtyDaysAgo},
+            })
+
+            for (let job of expiredJobs){
+                await strapi.entityService.update("api::job.job", job.attributes.jobAge.data.attributes.ageOfJob = "3")
+            }
+        }
+    },
+
+    options: {
+        rule: "0 0 * * *"
+    }
+}
