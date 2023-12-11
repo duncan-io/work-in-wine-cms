921aba5119a8cff4ee0d7c39da24477e8bd5c589
duncan <dmckee95@gmail.com>
2023-12-11 16:34:27 -0500

    Set up second CRON job for jobs that are 3 days old

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -1,5 +1,21 @@
 module.exports = {
+
+    // Change jobs from "New" to "Valid" after they have been live for 3 days, once every night at midnight
     "0 0 * * *": async ({strapi}) => {
+        const threeDaysAgo = new Date();
+        threeDaysAgo.setDate(thirtyDaysAgo.getDate() - 3)
+
+        const expiredJobs = await strapi.entityService.findMany("api::job.job", {
+           filters: {createdAt: three},
+        })
+
+        for (let job of expiredJobs){
+            await strapi.entityService.update("api::job.job", job.attributes.jobAge.data.attributes.ageOfJob = 2)
+        }
+    },
+
+    //Change jobs from "valid" to "expired" after they have been live for 30 days, once every night/morning at 1am
+    "0 1 * * *": async ({strapi}) => {
         const thirtyDaysAgo = new Date();
         thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30)
 
@@ -10,23 +26,6 @@ module.exports = {
         for (let job of expiredJobs){
             await strapi.entityService.update("api::job.job", job.attributes.jobAge.data.attributes.ageOfJob = 3)
         }
-
-    // myJob:{
-    //     task: async ({strapi}) => {
-    //         const thirtyDaysAgo = new Date();
-    //         thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30)
-
-    //         const expiredJobs = await strapi.entityService.findMany("api::job.job", {
-    //            filters: {createdAt: thirtyDaysAgo},
-    //         })
-
-    //         for (let job of expiredJobs){
-    //             await strapi.entityService.update("api::job.job", job.attributes.jobAge.data.attributes.ageOfJob = "3")
-    //         }
-    //     }
-    // },
-
-    // options: {
-    //     rule: "0 0 * * *",
     }
+    
 }
